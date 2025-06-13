1716343e465a2b5ea0bfaa4791b33102d6243e87
NamTaekHo <taekho1225@gmail.com>
2025-06-13 17:02:09 +0900

    conflict 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
-import Signup from './pages/auth/Signup';
+import Signup from "./pages/auth/Signup";
 import MemberManagement from "./pages/member/Member";
 import DeletedMember from "./pages/member/DeletedMember";
 import QnaManagment from "./pages/qna/QnaList";
 import QnaDetailPage from "./pages/qna/QnaDetail";
 import NoticeListPage from "./pages/notice/NoticeList";
 import NoticeDetailPage from "./pages/notice/NoticeDetail";
-import NoticeNewPage from './pages/notice/NoticeNewPage'; // NoticeNewPage 임포트
+import NoticeNewPage from "./pages/notice/NoticeNewPage"; // NoticeNewPage 임포트
 import ReportListPage from "./pages/report/ReportList";
 import ReportDetailPage from "./pages/report/ReportDetail";
 import SuspendedMemberManagement from "./pages/report/SuspendedMember";
@@ -16,12 +16,11 @@ import BannedMemberDetail from "./pages/report/BannedMemberDetail";
 import ProductListPage from "./pages/product/ProductList";
 import ItemListPage from "./pages/item/ItemList";
 import PaymentHistoryPage from "./pages/payment/PaymentHistory";
-import ChatRoomManagementPage from "./pages/chat/ChatRoomManagementPage";
+// import ChatRoomManagementPage from "./pages/chat/ChatRoomManagementPage";
 import ThemeManagementPage from "./pages/chat/ThemeManagement";
 import EventManagementPage from "./pages/chat/EventManagement";
 import SuspendedQnaListPage from "./pages/qna/SuspendedQnaList";
 import SuspendedQnaDetailPage from "./pages/qna/SuspendedQnaDetail"; // SuspendedQnaDetailPage 임포트
-
 
 export default function App() {
   return (
@@ -51,7 +50,7 @@ export default function App() {
       <Route path="/products" element={<ProductListPage />} />
       <Route path="/items" element={<ItemListPage />} />
       <Route path="/payments" element={<PaymentHistoryPage />} />
-      <Route path="/chatrooms" element={<ChatRoomManagementPage />} />
+      {/* <Route path="/chatrooms" element={<ChatRoomManagementPage />} /> */}
       <Route path="/themes" element={<ThemeManagementPage />} />
       <Route path="/events" element={<EventManagementPage />} />
       <Route path="/suspendedqnalist" element={<SuspendedQnaListPage />} />
